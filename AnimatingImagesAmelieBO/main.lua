2c81bce101605a083902602363f4fe48723b70ae
s436394 <47784278+s436394@users.noreply.github.com>
2019-02-28 15:53:17 -0500

    added background

--- AnimatingImagesAmelieBO/main.lua
+++ AnimatingImagesAmelieBO/main.lua
@@ -1,7 +1,39 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: AnimatingImages
+-- Name: AmelieBO
+-- Course: ICS2O/3C
+-- This program moves three images in different ways.
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with the width and height
+local backgroundImage = display.newImageRect("Images/ocean.jpg", 2048, 1536)
+
+-- characters image with the width and height
+local redfish = display.newImageRect("Images/redfish.png", 200, 200)
+local jelly = display.newImageRect("Images/jelly.png", 300, 300)
+local seahorse = display. newImage("Images/seahorse", 200, 200)
+
+-- set the image to be transparent
+redfish.alpha = 0
+
+-- set the initial x and y position of the images
+redfish.x = 0
+redfish.y = 100
+
+-- Function: Move MoveRedFish
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description This funtion adds the scroll speed to the x-value of the ship
+local function MoveRedFish(event)
+	-- add the scroll speed to the x-value of the ship
+	redfish.x = redfish.x + scrollSpeed
+	-- change the transparency everytime it move so it fades out
+	redfish.alpha = redfish.alpha + 0.01
+end
+
+-- MoveredFish will be called over and over again
+Runtime:addEventListener("enterFrame", MoveRedFish)
