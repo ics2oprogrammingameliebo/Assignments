f42bf9274dfb42f6f90653152b0315a4d8580d78
s436394 <amelie.bender-olivas@ocsbstudent.ca>
2019-02-28 17:54:43 -0500

    Update main.lua

--- AnimatingImagesAmelieBO/main.lua
+++ AnimatingImagesAmelieBO/main.lua
@@ -14,8 +14,7 @@ local backgroundImage = display.newImageRect("Images/ocean.jpg", 2048, 1536)
 
 -- characters image with the width and height
 local redfish = display.newImageRect("Images/redfish.png", 200, 200)
-local jelly = display.newImageRect("Images/jelly.png", 300, 300)
-local seahorse = display. newImage("Images/seahorse", 200, 200)
+local jelly = display.newImageRect("Images/jelly.png", 200, 200)
 
 -- set the image to be transparent
 redfish.alpha = 0
@@ -37,3 +36,25 @@ end
 
 -- MoveredFish will be called over and over again
 Runtime:addEventListener("enterFrame", MoveRedFish)
+----------------------------------------------------
+-- set the image to be transparent
+jelly.alpha = 0
+
+-- set the initial x and y position of the images
+jelly.x = 0
+jelly.y = 300
+
+
+-- Function: Move MoveJelly
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description This funtion adds the scroll speed to the x-value of the ship
+local function MoveJelly(event)
+	-- add the scroll speed to the x-value of the ship
+	jelly.x = jelly.x + scrollSpeed
+	-- change the transparency everytime it move so it fades out
+	jelly.alpha = jelly.alpha + 0.01
+end
+
+-- MoveredFish will be called over and over again
+Runtime:addEventListener("enterFrame", MoveJelly)
