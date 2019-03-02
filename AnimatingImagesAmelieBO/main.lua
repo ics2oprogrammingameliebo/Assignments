8dd6a74ce20df68f62a38c7e9cbe92fb04e50ce8
s436394 <amelie.bender-olivas@ocsbstudent.ca>
2019-03-02 11:36:58 -0500

    right, left and up

--- AnimatingImagesAmelieBO/main.lua
+++ AnimatingImagesAmelieBO/main.lua
@@ -7,7 +7,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- global variables
-scrollSpeed = 3
+scrollSpeed = 4
 
 -- background image with the width and height
 local backgroundImage = display.newImageRect("Images/ocean.jpg", 2048, 1536)
@@ -15,13 +15,27 @@ local backgroundImage = display.newImageRect("Images/ocean.jpg", 2048, 1536)
 -- characters image with the width and height
 local redfish = display.newImageRect("Images/redfish.png", 200, 200)
 local jelly = display.newImageRect("Images/jelly.png", 200, 200)
+local sea = display.newImageRect("Images/Sea.png", 200, 200)
 
+local areaText
+local textSize = 90
 
--- Flip the image vertically and horizontally
-jelly:scale( -1, -1 )
+-- display text on the screen.
+areaText = display.newText("Under the sea", 0,0, Consolas, textSize)
 
--- set the image to be transparent
-redfish.alpha = 0
+-- set the color of the newText
+areaText:setTextColor(255/255, 255/255, 255/255)
+
+-- anchor the text and set its (x.y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 220
+areaText.y = 20
+areaText = display.contentHeight/2
+
+--------------------------------------------------------------
+-- set the image to be visible
+redfish.alpha = 1
 
 -- set the initial x and y position of the images
 redfish.x = 0
@@ -44,12 +58,10 @@ end
 -- MoveredFish will be called over and over again
 Runtime:addEventListener("enterFrame", MoveRedFish)
 ----------------------------------------------------
--- set the image to be transparent
-jelly.alpha = 0
 
 -- set the initial x and y position of the images
-jelly.x = 0
-jelly.y = 300
+jelly.x = 500
+jelly.y = 900
 
 -- Function: Move MoveJelly
 -- Input: this function accepts an event listener
@@ -57,10 +69,29 @@ jelly.y = 300
 -- Description This funtion adds the scroll speed to the x-value of the ship
 local function MoveJelly(event)
 	-- add the scroll speed to the x-value of the ship
-	jelly.x = jelly.x + scrollSpeed
-	-- change the transparency everytime it move so it fades out
-	jelly.alpha = jelly.alpha + 0.01
+	jelly.y = jelly.y - scrollSpeed
 end
 
--- MoveredFish will be called over and over again
+-- MoverJelly will be called over and over again
 Runtime:addEventListener("enterFrame", MoveJelly)
+-------------------------------------------------
+-- set the image to be visible
+sea.alpha = 0
+
+-- set the initial x and y position of the images
+sea.x = 900
+sea.y = 500
+
+-- Function: Move MoveSea
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description This funtion adds the scroll speed to the x-value of the ship
+local function MoveSea(event)
+	-- add the scroll speed to the x-value of the ship
+	sea.x = sea.x - scrollSpeed
+	-- change the transparency everytime it move so it fades out
+	sea.alpha = sea.alpha + 0.01
+end
+
+-- MoveSea will be called over and over again
+Runtime:addEventListener("enterFrame", MoveSea)
