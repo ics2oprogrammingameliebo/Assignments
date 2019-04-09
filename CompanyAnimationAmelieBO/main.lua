4aed8b8ebcfbd13a123f86db0444099225941488
Amelie Bender Olivas <47784278+s436394@users.noreply.github.com>
2019-04-09 09:42:31 -0400

    3

--- CompanyAnimationAmelieBO/main.lua
+++ CompanyAnimationAmelieBO/main.lua
@@ -1,7 +1,16 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: CompanyLogoAnimation
+-- Name: AmelieBenderOlivas
+-- Course: ICS2O/3C
+-- This program animates my company logo.
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- set background colour
+display.setDefault("background", 1, 1, 1)
+
+-- character/ logo images with width and height
+local 
